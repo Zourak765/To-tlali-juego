9311a2ac4911fd622dbd96c4e75ff150cfa4f8dc
Zourak765 <cazourak@gmail.com>
2026-04-09 22:46:41 -0600

    Para sacar build
    Para sacar build

--- Assets/Project/Scripts/Minijuego Guitar/ZonaDestroy.cs
+++ Assets/Project/Scripts/Minijuego Guitar/ZonaDestroy.cs
@@ -1,4 +1,3 @@
-using UnityEditor.U2D.Aseprite;
 using UnityEngine;
 
 public class ZonaDestroy : MonoBehaviour
@@ -6,19 +5,18 @@ public class ZonaDestroy : MonoBehaviour
     public string tagNota;
     public string tagNota2;
     public MiniJuegoManager manager;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag(tagNota))
         {
             manager.NotaIncorrecta();
-
             Destroy(other.gameObject);
         }
-        
+
         if (other.CompareTag(tagNota2))
         {
             manager.NotaIncorrecta();
-
             Destroy(other.gameObject);
         }
     }
